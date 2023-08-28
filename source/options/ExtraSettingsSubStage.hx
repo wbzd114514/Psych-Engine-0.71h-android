cf4024e6c0ed5c937af067c67aacd5c259382bbc
wbzd114514 <125738003+wbzd114514@users.noreply.github.com>
2023-08-28 13:22:38 +0800

    Update ExtraSettingsSubStage.hx

--- source/options/ExtraSettingsSubStage.hx
+++ source/options/ExtraSettingsSubStage.hx
@@ -3,13 +3,16 @@ package options;
 class ExtraSettingsSubStage extends BaseOptionsMenu
 {
     public function new()
-	{
-		title = 'Extra Settings';
-		rpcTitle = 'Extra Settings Menu'; //for Discord Rich Presence
+    {
+	title = 'Extra Settings';
+	rpcTitle = 'Extra Settings Menu'; //for Discord Rich Presence
 
-		var option:Option = new Option('strawberry', 'If checked...wait,a strawberry???', 'strawberry', 'bool'); 
-		addOption(option);
+	var option:Option = new Option('strawberry', 
+				       'If checked...wait,a strawberry???', 
+				       'strawberry', 
+				       'bool'); 
+	addOption(option);
 		
-		super()
-	}
+	super()
+    }
 }
