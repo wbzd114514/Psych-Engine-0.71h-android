eff76d7acb2f6f7362eadaba04f86abf5ae0082b
wbzd114514 <125738003+wbzd114514@users.noreply.github.com>
2023-08-28 13:54:48 +0800

    Add files via upload

---
+++ source/options/ExtraSettingsSubState.hx
@@ -0,0 +1,15 @@
+package options;
+
+class ExtraSettingsSubStage extends BaseOptionsMenu
+{
+    public function new()
+	{
+		title = 'Extra Settings';
+		rpcTitle = 'Extra Settings Menu'; //for Discord Rich Presence
+
+		var option:Option = new Option('strawberry', 'If checked...wait,a strawberry???', 'strawberry', 'bool'); 
+		addOption(option);
+		
+		super();
+	}
+}
