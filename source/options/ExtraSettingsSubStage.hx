5b131ac11ef7782da397504d0ec0feb49188a889
wbzd114514 <125738003+wbzd114514@users.noreply.github.com>
2023-08-28 22:10:22 +0800

    Update ExtraSettingsSubStage.hx

--- source/options/ExtraSettingsSubStage.hx
+++ source/options/ExtraSettingsSubStage.hx
@@ -8,11 +8,17 @@ class ExtraSettingsSubStage extends BaseOptionsMenu
 	rpcTitle = 'Extra Settings Menu'; //for Discord Rich Presence
 
 	var option:Option = new Option('strawberry', 
-				       'If checked...wait,a strawberry???', 
-				       'strawberry', 
+				       'If checked...wait,a strawberry???/nPS:it will change the Auto Pause!', 
+				       'autoPause', 
 				       'bool'); 
 	addOption(option);
+	option.onChange = onChangeAutoPause;
 		
 	super();
+    }
+
+    function onChangeAutoPause()
+    {
+             FlxG.autoPause = ClientPrefs.data.autoPause;
     }
 }
